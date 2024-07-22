4fba8219deab2e2da334a12cbb1a3ab847da9688
Sankhadip Pal <saffron450@gmail.com>
2024-07-22 12:17:10 +0530

    stripe debugs

--- components/payment-form/payment-form.component.jsx
+++ components/payment-form/payment-form.component.jsx
@@ -1,86 +1,71 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+// import { useSelector } from 'react-redux';
 
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+// import { selectCartTotal } from '../../store/cart/cart.selector';
+// import { selectCurrentUser } from '../../store/user/user.selector';
+
+import { FormContainer } from './payment-form.styles';
+import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  // const amount = useSelector(selectCartTotal);
+  // const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsProcessingPayment(true);
+    const response = await fetch(`${process.env.STRIPE_SECRET_KEY}/.netlify/functions/create-payment-intent`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json({url: session.url}));
+    console.log(response);
+    // const { client_secret: clientSecret } = response.paymentIntent;
+
+    // const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+    //   payment_method: {
+    //     card: elements.getElement(CardElement),
+    //     billing_details: {
+    //       name: currentUser ? currentUser.displayName : 'Yihua Zhang',
+    //     },
+    //   },
+    // });
+
+    // setIsProcessingPayment(false);
+
+  //   if (paymentResult.error) {
+  //     alert(paymentResult.error.message);
+  //   } else {
+  //     if (paymentResult.paymentIntent.status === 'succeeded') {
+  //       alert('Payment Successful!');
+  //     }
+  //   }
+  };
+
   return (
-    <div>
-      <CardElement/>
-      {/* <Button buttonType={BUTTON_TYPE_CLASSES}> Pay Now</Button> */}
-    </div>
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <PaymentButton
+          buttonType={BUTTON_TYPE_CLASSES}
+          // isLoading={isProcessingPayment}
+        >
+          Pay Now
+        </PaymentButton>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
-
 export default PaymentForm;
-// import { useState } from 'react';
-// import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-// // import { useSelector } from 'react-redux';
-// // import { selectCartTotal } from '../../store/cart/cart.selector';
-// // import { selectCurrentUser } from '../../store/user/user.selector';
-
-// import { FormContainer } from './payment-form.styles';
-// import { BUTTON_TYPE_CLASSES } from '../button/button.component';
-
-// import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
-
-// const PaymentForm = () => {
-//   const stripe = useStripe();
-//   const elements = useElements();
-// //   const amount = useSelector(selectCartTotal);
-// //   const currentUser = useSelector(selectCurrentUser);
-//   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
-
-//   const paymentHandler = async (e) => {
-//     e.preventDefault();
-//     if (!stripe || !elements) {
-//       return;
-//     }
-//     setIsProcessingPayment(true);
-//     const response = await fetch('/.netlify/functions/create-payment-intent', {
-//       method: 'post',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ amount: 10000 }),
-//     }).then((res) => {
-//       return res.json();
-//     });
-
-//     const clientSecret = response.paymentIntent.client_secret;
-
-//     const paymentResult = await stripe.confirmCardPayment(clientSecret, {
-//       payment_method: {
-//         card: elements.getElement(CardElement),
-//         billing_details: {
-//           name: currentUser ? currentUser.displayName : 'Yihua Zhang',
-//         },
-//       },
-//     });
-
-//     setIsProcessingPayment(false);
-
-//     if (paymentResult.error) {
-//       alert(paymentResult.error.message);
-//     } else {
-//       if (paymentResult.paymentIntent.status === 'succeeded') {
-//         alert('Payment Successful!');
-//       }
-//     }
-//   };
-
-//   return (
-//     <PaymentFormContainer>
-//       <FormContainer onSubmit={paymentHandler}>
-//         <h2>Credit Card Payment:</h2>
-//         <CardElement />
-//         <PaymentButton
-//           buttonType={BUTTON_TYPE_CLASSES}
-//           // isLoading={isProcessingPayment}
-//         >
-//           Pay Now
-//         </PaymentButton>
-//       </FormContainer>
-//     </PaymentFormContainer>
-//   );
-// };
-// export default PaymentForm;
--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -2,17 +2,31 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Allow from any origin
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS', // Allow only POST requests
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ message: 'Preflight CORS check' }),
+    };
+  }
   try {
     const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "rs",
+      currency: "usd",
       payment_method_types: ["card"],
     });
 
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
@@ -20,6 +34,7 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 400,
+      headers,
       body: JSON.stringify({ error }),
     };
   }
